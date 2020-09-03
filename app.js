1fdc80af8cb8cf0751c013086fe943b13c52f0f6
Leo <dev@circular.is>
2020-09-03 11:51:08 +0200

    First Upload

---
+++ app.js
@@ -0,0 +1,10 @@
+/* © CIRCULAR AGENCY */
+
+// When the user clicks on div, open the popup
+function heroesPopup() {
+  var popup = document.getElementById("heroes__popup--01");
+  popup.classList.toggle("show");
+
+var popup = document.getElementById("heroes__brightness");
+  popup.classList.toggle("bright");
+}
