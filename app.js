e6cd4c16c0ea30c30a6c5e596d0ad192e24be32f
Leo <dev@circular.is>
2020-09-08 15:51:09 +0200

    Update

--- app.js
+++ app.js
@@ -2,8 +2,30 @@
 
 // When the user clicks on div, open the popup
 function heroesPopup() {
-  var popup = document.getElementById("heroes__popup--01");
-  popup.classList.toggle("show");
+  var btnGroup = Array.from(document.getElementsByClassName("btn"));
+
+/* Collect all .img classes into a NodeList and convert it into
+|| an array
+*/
+var imgGroup = Array.from(document.getElementsByClassName('img'));
+
+/* Iterate (loop) thru the imgGroup array with forEach() array
+|| method.
+*/// In each loop get a .btn from the btnGroup array's index
+//// position that corresponds with the current index of the loop.
+//// Register an onclick event handler that toggles the .off class
+//// to a .img of the imgGroup array positioned at current loop
+//// index.
+
+
+imgGroup.forEach(function(img, idx) {
+
+  btnGroup[idx].onclick = function() {
+
+    img.classList.toggle('show');
+  }
+
+});
 
 var popup = document.getElementById("heroes__brightness");
   popup.classList.toggle("bright");
