216d7df397d8c8d62071e23d1770a9a1c7282453
Leo <dev@circular.is>
2020-09-10 18:06:19 +0200

    Major Update

--- app.js
+++ app.js
@@ -1,32 +1,99 @@
 /* © CIRCULAR AGENCY */
 
 // When the user clicks on div, open the popup
-function heroesPopup() {
-  var btnGroup = Array.from(document.getElementsByClassName("btn"));
-
-/* Collect all .img classes into a NodeList and convert it into
-|| an array
-*/
-var imgGroup = Array.from(document.getElementsByClassName('img'));
-
-/* Iterate (loop) thru the imgGroup array with forEach() array
-|| method.
-*/// In each loop get a .btn from the btnGroup array's index
-//// position that corresponds with the current index of the loop.
-//// Register an onclick event handler that toggles the .off class
-//// to a .img of the imgGroup array positioned at current loop
-//// index.
-
-
-imgGroup.forEach(function(img, idx) {
-
-  btnGroup[idx].onclick = function() {
-
-    img.classList.toggle('show');
-  }
-
-});
+function heroesPopup1() {
+  var popup = document.getElementById("heroes__popup--01");
+  popup.classList.toggle("show");
 
 var popup = document.getElementById("heroes__brightness");
-  popup.classList.toggle("bright");
+popup.classList.toggle("bright");
+}
+
+function heroesPopup2() {
+  var popup = document.getElementById("heroes__popup--02");
+  popup.classList.toggle("show");
+
+
+var popup = document.getElementById("heroes__brightness");
+popup.classList.toggle("bright");
+}
+
+function heroesPopup3() {
+  var popup = document.getElementById("heroes__popup--03");
+  popup.classList.toggle("show");
+
+
+
+var popup = document.getElementById("heroes__brightness");
+popup.classList.toggle("bright");
+}
+
+function heroesPopup4() {
+  var popup = document.getElementById("heroes__popup--04");
+  popup.classList.toggle("show");
+
+
+
+var popup = document.getElementById("heroes__brightness");
+popup.classList.toggle("bright");
+}
+
+function heroesPopup5() {
+  var popup = document.getElementById("heroes__popup--05");
+  popup.classList.toggle("show");
+
+
+
+var popup = document.getElementById("heroes__brightness");
+popup.classList.toggle("bright");
+}
+
+function heroesPopup6() {
+  var popup = document.getElementById("heroes__popup--06");
+  popup.classList.toggle("show");
+
+
+
+var popup = document.getElementById("heroes__brightness");
+popup.classList.toggle("bright");
+}
+
+function heroesPopup7() {
+  var popup = document.getElementById("heroes__popup--07");
+  popup.classList.toggle("show");
+
+
+
+var popup = document.getElementById("heroes__brightness");
+popup.classList.toggle("bright");
+}
+
+function heroesPopup8() {
+  var popup = document.getElementById("heroes__popup--08");
+  popup.classList.toggle("show");
+
+
+
+var popup = document.getElementById("heroes__brightness");
+popup.classList.toggle("bright");
+}
+
+function heroesPopup9() {
+  var popup = document.getElementById("heroes__popup--09");
+  popup.classList.toggle("show");
+
+
+
+var popup = document.getElementById("heroes__brightness");
+popup.classList.toggle("bright");
+}
+
+function heroesPopup10() {
+  var popup = document.getElementById("heroes__popup--10");
+  popup.classList.toggle("show");
+
+
+
+var popup = document.getElementById("heroes__brightness");
+popup.classList.toggle("bright");
 }
