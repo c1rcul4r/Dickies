cc29a499afeb71a0f9e84d04586146a2d501deb0
Circular Agency <leo@circular.is>
2021-03-15 19:41:43 +0100

    Major

--- website/common/makers-grid/app.makers-grid.js
+++ website/common/makers-grid/app.makers-grid.js
@@ -1,4 +1,6 @@
 /* © CIRCULAR AGENCY */
+
+// MAKERS-GRID JS
 function heroesPopup1(){var popup=document.getElementById("popup--01");popup.classList.toggle("show");var popup=document.getElementById("brightness");popup.classList.toggle("bright")}
 function heroesPopup2(){var popup=document.getElementById("popup--02");popup.classList.toggle("show");var popup=document.getElementById("brightness");popup.classList.toggle("bright")}
 function heroesPopup3(){var popup=document.getElementById("popup--03");popup.classList.toggle("show");var popup=document.getElementById("brightness");popup.classList.toggle("bright")}
@@ -9,3 +11,5 @@ function heroesPopup7(){var popup=document.getElementById("popup--07");popup.cla
 function heroesPopup8(){var popup=document.getElementById("popup--08");popup.classList.toggle("show");var popup=document.getElementById("brightness");popup.classList.toggle("bright")}
 function heroesPopup9(){var popup=document.getElementById("popup--09");popup.classList.toggle("show");var popup=document.getElementById("brightness");popup.classList.toggle("bright")}
 function heroesPopup10(){var popup=document.getElementById("popup--10");popup.classList.toggle("show");var popup=document.getElementById("brightness");popup.classList.toggle("bright")}
+function heroesPopup11(){var popup=document.getElementById("popup--11");popup.classList.toggle("show");var popup=document.getElementById("brightness");popup.classList.toggle("bright")}
+function heroesPopup12(){var popup=document.getElementById("popup--12");popup.classList.toggle("show");var popup=document.getElementById("brightness");popup.classList.toggle("bright")}
